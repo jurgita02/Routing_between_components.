d4b4ffe76eea20ea587916029d4059ea580564a0
Jurgita Butiene <jurgita02@yahoo.com>
2023-08-01 16:21:02 +0200

    created build folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import FloraFauna from './FloraFauna/FloraFauna';
 import { BrowserRouter } from 'react-router-dom';
